a5a69c920f12da7ccf4df7ca5f46a71ff98c73b5
Dijellyman <rolest@live.com>
2019-09-30 20:00:50 -0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@
     <div class="container">
     <h2>Follow each link to the page example</h2>
     <ul class="nav nav-pills">
-        <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="Index.php">Home</a></li>
         <li class="nav-item"><a class="nav-link" href="array.php">Simple Array and Printouts</a></li>
         <li class="nav-item"><a class="nav-link" href="forloop.php">Simple For Loop</a></li>
         <li class="nav-item"><a class="nav-link" href="whiledowhileloop.php">Simple  While/Do While Loop</a></li>
